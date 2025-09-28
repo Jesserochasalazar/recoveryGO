064a0acf666d5246cd4b342148ed65c65d755eda
Jesse Rocha-salazar <jrochasalazar@ggc.edu>
2025-09-28 06:42:30 -0400

    Temp Working sign in, bottom nav, screens

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Tabs } from 'expo-router';
+
+
+export default function TabLayout() {
+  return (
+   <Tabs
+  screenOptions={{
+    tabBarActiveTintColor: '#ffd33d',
+    headerStyle: {
+      backgroundColor: '#25292e',
+    },
+    headerShadowVisible: false,
+    headerTintColor: '#fff',
+    tabBarStyle: {
+      backgroundColor: '#25292e',
+    },
+  }}
+>
+      {/* <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      /> */}
+       <Tabs.Screen name="dashboard" options={{ title: 'Dashboard', tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline' } color={color} size={24}/> ),}}
+        />
+        <Tabs.Screen name="progress" options={{ title: 'Progress', tabBarIcon: ({ color, focused }) =>  
+        (<Ionicons name={focused ? 'trending-up' : 'trending-up-outline'} color={color} size={24}/>
+        ),}}/>
+        <Tabs.Screen name="plans" options={{ title: 'Plans', tabBarIcon: ({ color, focused }) =>  
+        (<Ionicons name={focused ? 'clipboard' : 'clipboard-outline'} color={color} size={24}/>
+        ),}}/>
+        <Tabs.Screen name="settings" options={{ title: 'Settings', tabBarIcon: ({ color, focused }) =>  
+        (<Ionicons name={focused ? 'settings-sharp' : 'settings-outline'} color={color} size={24}/>
+        ),}}/>
+
+
+    </Tabs>
+    
+  );
+}
+
+
+//      <Tabs.Screen name="dashboard" options={{ title: 'Dashboard' }} />
---
+++ app/(tabs)/plans.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function PlansScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Plans Screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
+      <Link href="/" style={styles.button}>
+        Go to Home screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
